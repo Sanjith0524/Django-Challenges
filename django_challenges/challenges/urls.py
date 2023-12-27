f2fc473b46f909bf818363dbbbce5bd533c57f9d
Sanjith0524 <sanjith.m2023@vitstudent.ac.in>
2023-12-27 14:49:23 +0530

    FINAL

--- django_challenges/challenges/urls.py
+++ django_challenges/challenges/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-     path('')
+     path('',views.index,name='index') ,
      path('<int:month>',views.monthly_numbers) ,
      path('<str:month>',views.monthly_challenges,name='month-challenge')
 ]
--- django_challenges/challenges/views.py
+++ django_challenges/challenges/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse , HttpResponseNotFound ,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from  django.urls import reverse
-def index(request):
-    list_items = ''
-    months = list(monthly_chalenges.keys())
-
 monthly_chalenges = {
     'january' : 'Eat no meat for the entire month ',
     'february' : "Exercise for atleast 1 hour ",
@@ -16,18 +12,23 @@ monthly_chalenges = {
     'august' : "Exercise for atleast 1 hour " ,
     'september' : 'Avoid junk' ,
     'october' : 'Practice Django' ,
-    'november' : 'Practice Java' ,
+    'november' : None ,
     'december' : 'Eat no meat for the entire month '
 }
+
+def index(request):
+    months = list(monthly_chalenges.keys())
+    return render(request,'challenges/index.html',{'month':months})
+
+
 def monthly_numbers (request,month):
     months = list(monthly_chalenges.keys())
     forward_month = months[month-1]
     redirect_path = reverse('month-challenge',args=[forward_month])
-    return HttpResponseRedirect('/challenges/' + forward_month)
+    return HttpResponseRedirect(redirect_path)
 def monthly_challenges(request,month):
     try:
         challenge_text = monthly_chalenges[month]
-        response_data = f'<h1>{challenge_text}</h1>'
-        return HttpResponse(response_data)
+        return render(request,"challenges/challenge.html",{'text':challenge_text,'months':month})
     except:
-        return HttpResponseNotFound('Error 404')
+        return render(request,'404.html')
